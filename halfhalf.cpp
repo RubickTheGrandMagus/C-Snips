d0d4c407e671269cbe22902a4555b26376cf07da
PickleRick <40783878+RubickTheGrandMagus@users.noreply.github.com>
2022-12-08 13:37:47 +0800

    does something when reached median

---
+++ halfhalf.cpp
@@ -0,0 +1,26 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+int main() {
+    // Write C++ code here
+    int cnt = 10;
+    int dv = 2;
+    int rn = cnt/dv;
+    int mn = cnt-rn;
+    for(int i=0; i<cnt; i++){
+        if(i<rn){
+            std::cout << i;
+            std::cout << "\n";
+        }
+        else{
+            i--;
+            rn = rn + (mn/dv);
+            mn = mn-(mn/dv);
+            
+            //std::cout << rn;
+            std::cout << "\n";
+        }
+    }
+
+    return 0;
+}
